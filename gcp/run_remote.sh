2fd263386829d52cdd6acca0deec597b507119fa
Stanislav Vitko <stas@vitko.me>
2018-08-03 00:40:23 -0700

    updates in run_remote

--- gcp/run_remote.sh
+++ gcp/run_remote.sh
@@ -3,7 +3,7 @@ set -x
 set -v
 set -e
 
-JOB_NAME=jiratrain_lin_simple_05
+JOB_NAME=jiratrain_lin_simple_08
 REGION=us-central1
 
 BUCKET_PATH=$BUCKET_NAME/ml
@@ -17,6 +17,21 @@ EVAL_DATA=gs://$BUCKET_PATH/data/eval.csv
 
 OUTPUT_PATH=gs://$BUCKET_PATH/$JOB_NAME
 
+#gcloud ml-engine jobs submit training $JOB_NAME \
+#    --stream-logs \
+#    --job-dir $OUTPUT_PATH \
+#    --runtime-version 1.8 \
+#    --module-name trainer.task \
+#    --package-path trainer/ \
+#    --region $REGION \
+#    --scale-tier STANDARD_1 \
+#    -- \
+#    --train-files $TRAIN_DATA \
+#    --eval-files $EVAL_DATA \
+#    --train-steps 5000 \
+#    --eval-steps 100 \
+#    --verbosity DEBUG
+
 gcloud ml-engine jobs submit training $JOB_NAME \
     --stream-logs \
     --job-dir $OUTPUT_PATH \
@@ -24,10 +39,9 @@ gcloud ml-engine jobs submit training $JOB_NAME \
     --module-name trainer.task \
     --package-path trainer/ \
     --region $REGION \
-    --scale-tier STANDARD_1 \
     -- \
     --train-files $TRAIN_DATA \
     --eval-files $EVAL_DATA \
-    --train-steps 10000 \
+    --train-steps 5000 \
     --eval-steps 100 \
     --verbosity DEBUG
