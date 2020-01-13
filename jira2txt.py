4a1d4931c6569eda19cd383f5f2184cc4b8bd666
Stanislav Vitko <stas@vitko.me>
2020-01-12 18:57:06 -0800

    add jira2txt.py

---
+++ jira2txt.py
@@ -0,0 +1,30 @@
+import os
+
+import hashlib
+
+from preprocessor import process_dir
+
+def md5(text: str):
+    h = hashlib.new("md5")
+    h.update(text.encode("utf8"))
+    return h.hexdigest()
+
+def main():
+    src_dir = "dump/"
+    dst_dir = "jita_texts"
+
+    os.makedirs(dst_dir, exist_ok=True)
+
+    data = process_dir(src_dir)
+
+    for issue in data:
+        text = issue["text"]
+        digest = md5(text)
+
+        dst = os.path.join(dst_dir, digest + ".txt")
+        with open(dst, "w", encoding="utf8") as dst_file:
+            dst_file.write(text)
+
+
+if __name__ == '__main__':
+    main()
